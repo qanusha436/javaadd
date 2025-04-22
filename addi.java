190ec62a5b39c37e8b8aeb22b5fc49f486364955
qanusha436 <qanusha436@gmail.com>
2025-04-22 12:43:28 +0530

    saved

--- addi.java
+++ addi.java
@@ -2,7 +2,10 @@ public class addi {
     public static void main(String[] args) {
         int a=6,b=5;
         int sum=a+b;
+        int sub=a-b;
         System.out.println("addition of two numbers:"+sum);
+        System.out.println("substration of two numbers:"+sub);
+        
         
     }
     
