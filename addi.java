bae9137d0eb234df8bf4f04d331d9a358ab96feb
qanusha436 <qanusha436@gmail.com>
2025-04-22 12:31:50 +0530

    save

---
+++ addi.java
@@ -0,0 +1,9 @@
+public class addi {
+    public static void main(String[] args) {
+        int a=6,b=5;
+        int sum=a+b;
+        System.out.println("addition of two numbers:"+sum);
+        
+    }
+    
+}
